45b0d0a4791741709793ceeb35e9470a453a31f2
Celso Bastos <celsorico@gmail.com>
2026-03-10 22:59:55 -0300

    Bootstrap

---
+++ js/java-script.js
@@ -0,0 +1,21 @@
+// console.log("helo world!")
+
+
+// Criação de variaveis
+var nome;
+var enderecoEmpresa;
+var cep;
+var salario;
+var contrato;
+var complemento; //undefined
+
+nome = "Impacta"; // String
+enderecoEmpresa = "Rua das flores, 455"; // String 
+cep = "124555";
+salario = 412.4; // number
+contrato = true; // Bollean
+complemento = null; // null
+
+
+console.log(complemento)
+
